88d409d23f629d458b4282e0cd541f3871d6c21c
Alyssa Pratt <snubly1@gmail.com>
2016-07-05 14:12:47 -0400

    added tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705175020) do
+ActiveRecord::Schema.define(version: 20160705180928) do
+
+  create_table "cats", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "breed"
+    t.integer  "age"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string   "amount"
+    t.integer  "cat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
