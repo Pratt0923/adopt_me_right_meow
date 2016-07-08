3640815a1fb6abb94b3bee4888ad42b908461b97
Spoonflower <mhowell@spoonflower.com>
2016-07-08 15:15:28 -0400

    taking out comments

--- spec/controllers/transaction_controller_spec.rb
+++ spec/controllers/transaction_controller_spec.rb
@@ -1,24 +1,2 @@
 require 'rails_helper'
 require 'spec_helper'
-
-
-# need this to pass
-# class TransactionController < ApplicationController
-#   def index
-#     @transactions = Transaction.all
-#      respond_to do |format|
-#       format.html
-#       format.csv { send_data @transactions.to_csv, filename: "transactions-#{Date.today}.csv" }
-#     end
-#   end
-# end
-
-
-# describe TransactionController do
-#   describe '#index' do
-#     it 'should be able to send an csv file' do
-#        # stubs and expectations go here
-#        get :index, :format => 'csv'
-#     end
-#   end
-# end
