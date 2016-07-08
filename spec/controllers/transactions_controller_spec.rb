356dc75090fbd5e5a6b5ca8c49583dc644a88c4e
Spoonflower <mhowell@spoonflower.com>
2016-07-08 13:43:19 -0400

    adding comments to spec controllers

--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -1,2 +1,21 @@
 require 'rails_helper'
 require 'spec_helper'
+
+
+# need this to pass
+# class TransactionController < ApplicationController
+#   def index
+#     @transactions = Transaction.all
+#      respond_to do |format|
+#       format.html
+#       format.csv { send_data @transactions.to_csv, filename: "transactions-#{Date.today}.csv" }
+#     end
+#   end
+# end
+
+
+describe TransactionController do
+  it '' do
+
+  end
+end
