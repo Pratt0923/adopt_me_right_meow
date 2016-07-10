720a67a915b6887ff7d2651ed1e4ab9179876695
Spoonflower <mhowell@spoonflower.com>
2016-07-10 17:06:13 -0400

    things

--- spec/controllers/charges_controller_spec.rb
+++ spec/controllers/charges_controller_spec.rb
@@ -1,39 +1,19 @@
 require 'rails_helper'
 require 'spec_helper'
 
-# class ChargesController < ApplicationController
-#   def new
-#     @cat = Cat.find(params[:cat_id])
-#   end
-#    def create
-#     @cat = Cat.find(params[:cat_id])
-#     @amount = @cat.price
-#      customer = Stripe::Customer.create(
-#       :email => params[:stripeEmail],
-#       :source  => params[:stripeToken]
-#     )
-# #we need to change our token because apparently you cant use a stripe token more then once. stupid rules
-#     charge = Stripe::Charge.create(
-#       :customer    => customer.id,
-#       :amount      => @amount.to_i,
-#       :description => 'Rails Stripe customer',
-#       :currency    => 'usd'
-#     )
-#    rescue Stripe::CardError => e
-#     flash[:error] = e.message
-#     redirect_to new_charge_path
-#   end
-# end
-
 describe ChargesController do
-  it 'can add users to the subscription list' do
-    user = create :user
-    sign_in user
-    post :create, {
-      :subscribe => "yes"
-    }
-    user.reload
-    expect(user.subscription).to eq(true)
+  # it 'can add users to the subscription list' do
+  #   user = create :user
+  #   sign_in user
+  #   post :create, {
+  #     :subscribe => "yes"
+  #   }
+  #   user.reload
+  #   expect(user.subscription).to eq(true)
+
+  # end
+
+  it 'will see error if something wrong with card' do
 
   end
 
