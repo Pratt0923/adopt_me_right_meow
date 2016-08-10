bc11cc2f4202d2db95c64a96c1e130fa5f6b8f3c
Alyssa Pratt <snubly1@gmail.com>
2016-08-10 09:10:37 -0400

    removed random part of schema that is not used anymore but is still being genreated for some reason

--- db/schema.rb
+++ db/schema.rb
@@ -22,11 +22,6 @@ ActiveRecord::Schema.define(version: 20160706193431) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "controllers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "emails", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
