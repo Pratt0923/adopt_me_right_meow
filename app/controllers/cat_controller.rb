778edc736fc5cd2896337df211e35dd0a0b97c99
Alyssa Pratt <snubly1@gmail.com>
2016-08-10 09:09:44 -0400

    added validation on the csv files

--- app/controllers/cat_controller.rb
+++ app/controllers/cat_controller.rb
@@ -6,20 +6,31 @@ class CatController < ApplicationController
   end
 
   def import
-    Cat.import(params[:file])
-    redirect_to root_path, notice: "Products imported."
+    if params[:file].tempfile.first == "name,description,breed,age,price\n"
+      Cat.import(params[:file])
+      redirect_to root_path, notice: "Cats imported."
+    else
+      flash[:danger] = "That is not the correct format!"
+    end
   end
 
+
+# TODO validation check goes here
   def create
-    @cat = Cat.new(
-    name: params[:cat][:name],
-    description: params[:cat][:description],
-    breed: params[:cat][:breed],
-    age: params[:cat][:age].to_i,
-    price: params[:cat][:price]
-    )
-    @cat.save
-    redirect_to root_path, notice: "Cat Added!"
+    if current_user.admin == true
+      @cat = Cat.new(
+        name: params[:cat][:name],
+        description: params[:cat][:description],
+        breed: params[:cat][:breed],
+        age: params[:cat][:age].to_i,
+        price: params[:cat][:price]
+      )
+      #params.require(:cat).permit(:name, :description, :breed, :age, :price)
+      @cat.save
+      redirect_to root_path, notice: "Cat Added!"
+    else
+      flash[:danger] = "You are not allowed to do that!"
+    end
   end
 
   def destroy
--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -2,11 +2,14 @@ class EmailController < ApplicationController
   def index
   end
 
+  #if subscribe == yes then heroku will send an email out to the email that is subscribed once a week on Monday.
   def create
     if params[:subscribe] == "yes"
       user = current_user
       user.subscription = true
       user.save!
+    else
+      flash[:success] = "Your subscription preferences have been updated!"
     end
   end
 end
